3ad0909685f18b0bc504ef3d0468bfb3eb203f92
chareef <howlong53@gmail.com>
2017-09-20 15:22:19 +0700

    hjk

--- application/controllers/backend/Login.php
+++ application/controllers/backend/Login.php
@@ -2,9 +2,27 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class login extends CI_Controller{
+
 	public function memberLogin()
 	{
-		$tests = array('test'=>true);
-		echo json_encode($tests);
+		$data = json_decode(file_get_contents('php://input'),true);
+		$email = $data['email'];
+		$pass = $data['pass'];
+		$this->db->from('member');
+		$this->db->where('member_email',$email);
+		$this->db->where('member_pass',$pass);
+		$query = $this->db->get();
+		$count = $query->num_rows();
+		if($count > 0)
+		{
+			foreach ($query->result() as $row)
+			{			    
+				echo json_encode(array('statusLogin'=>true,'memberCode'=>$row->member_code));
+			}
+		}
+		else 
+		{
+			echo json_encode(array('statusLogin'=>false));
+		}
 	}
 }
